60de48ab9c923b7881a4c0a3297c588d2b1a8848
Guillermo <guillermo.rabassa@educa.madrid.org>
2024-02-15 20:25:02 +0100

    Añadida documentación al código.

--- scripts/calendario.js
+++ scripts/calendario.js
@@ -1,17 +1,26 @@
+/**
+ * Crea un calendario con el mes al que pertenece el día
+ * @param {Día del mes a crear} dia 
+ * @returns Tabla de calendario
+ */
 function crearMes(dia){
+    // Escribe el nombre del mes en español encima del calendario
     document.getElementById("labelCal").innerText = mayusMinus(
         dia.toLocaleString('default', { month: 'long' }));
-
+    
+    // Crea la tabla con columna
     let tablaMes = document.createElement("table");
     let mes = dia.getMonth();
     let col = document.createElement("tr");
 
+    // Para que esté correctamente alineado, añade huecos si el mes empieza en
+    // un día diferente al lunes
     for (let i = 1; i < (dia.getDay()==0 ? 7 : dia.getDay()); i++) {
         let tdDia = document.createElement("td");
-        
         col.appendChild(tdDia);
     }
 
+    // Mientras que el día siga siendo parte del mes actual, añadirlo a la tabla
     while(mes===dia.getMonth()){
         
         let botonDia = document.createElement("input");
@@ -25,32 +34,45 @@ function crearMes(dia){
 
         botonDia.classList.add("diaCalendario");
 
+        // Añade el botón del día al td, después a la columna 
+        // y por último adelanta un día
         tdDia.appendChild(botonDia);
         col.appendChild(tdDia);
         dia.setDate(dia.getDate()+1);
 
-
+        // Si el día de la semana es lunes, iniciar nueva columna
         if(dia.getDay()===1){
             tablaMes.appendChild(col);
             col = document.createElement("tr");
         }
     }
+    
     tablaMes.appendChild(col);
     tablaMes.style.tableLayout = "fixed";
     return tablaMes;
 }
 
+/**
+ * @param {Texto a estilizar} str 
+ * @returns Devuelve el texto con primera mayúscula y el resto a minúsculas
+ */
 function mayusMinus(str){
     return str.charAt(0).toUpperCase()+str.substring(1);
 }
 
-const diaActual = new Date();
+// Si la página es el índice
 const index = (window.location.pathname).split("/").pop() == "index.html" ? true : false;
+
+const diaActual = new Date();
 const calendar = document.getElementById("calendar");
 
 var primerDia = new Date(diaActual.getFullYear(), diaActual.getMonth(), 1);
 var mes = diaActual.getMonth();
 
+/**
+ * Cambia el mes del calendario
+ * @param {Si debe ir hacía delante o detrás} mas 
+ */
 function cambiarMes(mas){
     mes+=(mas ? +1 : -1);
     primerDia = new Date(primerDia.getFullYear(), mes, 1);
@@ -58,6 +80,10 @@ function cambiarMes(mas){
     buscarActiv();
 }
 
+/**
+ * Colorea dentro del calendario aquellos días que tengan actividades,
+ * usando el id de las actividades del índice.
+ */
 function buscarActiv(){
     if (index) {
         let dias = document.getElementsByClassName("diaCalendario");
@@ -76,5 +102,6 @@ function buscarActiv(){
     }
 }
 
+// Inicializa el calendario
 calendar.appendChild(crearMes(primerDia));
 buscarActiv();
--- scripts/registro.js
+++ scripts/registro.js
@@ -1,8 +1,8 @@
-var selHora = document.getElementById('horaDisp');
+var selHora = document.getElementById('horaDisp'); 
 
 for (var i = 8; i<=21; i++){
-    var hora = document.createElement('option');
-    var media = document.createElement('option');
+    var hora = document.createElement('option'); // Cada hora redonda
+    var media = document.createElement('option'); // Cada media hora
     hora.value = i;
     hora.innerHTML = i + ":00";
     media.value = i + 0.5;
@@ -11,12 +11,19 @@ for (var i = 8; i<=21; i++){
     selHora.appendChild(media);
 }
 
-function setEsCivil(div){
-    let divs = document.getElementsByClassName("labelEstado");
-    for(let i = 0; i < divs.length; i++) {
-        divs[i].style = "background-color : red";
+
+/**
+ * Colorea las etiquetas del estado civil acorde a su estado
+ * @param {Etiqueta de estado civil a seleccionar} label 
+ */
+function setEsCivil(label){
+    let labels = document.getElementsByClassName("labelEstado");
+
+    for(let i = 0; i < labels.length; i++) {
+        labels[i].style = "background-color : red"; 
+        // Todas las etiquetas coloreadas como deseleccionadas
     }
 
-    div.style = "background-color : green";
-
+    label.style = "background-color : green";
+    // La etiqueta seleccionada coloreada acorde
 }
