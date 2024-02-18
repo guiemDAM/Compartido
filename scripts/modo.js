66d8817d5a9ae0e808a60d4d399a15dcb9f27306
guiem <guillermo.rabassa@educa.madrid.org>
2024-02-18 15:39:44 +0100

    Añadido botón y funcionalidad para el cambio de tema.

--- scripts/modo.js
+++ scripts/modo.js
@@ -1,8 +1,11 @@
 var isModoOscuro = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+const btnModo = document.getElementById('boton_modo');
+
 function applyModo() {
     document.documentElement.classList.add("modo_" + (isModoOscuro ? "oscuro" : "claro"));
     document.documentElement.classList.remove("modo_" + (isModoOscuro ? "claro" : "oscuro"));
+    btnModo.innerHTML = isModoOscuro ? "&#9789" : "&#x2600";
 }
 
 function switchModo() {
@@ -11,3 +14,6 @@ function switchModo() {
 }
 
 applyModo(); // Aplicar modo al cargar la página
+
+btnModo.addEventListener('click', switchModo);
+
